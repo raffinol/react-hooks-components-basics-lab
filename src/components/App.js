707179d7b430147453f726138151076698bc9885
Rafael Finol <rafaelfinol88@gmail.com>
2023-05-24 22:33:42 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect } from 'react';
 
 function NavBar() {
   return (
@@ -16,15 +16,20 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id="about">
+      <h2 id="about">About</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
